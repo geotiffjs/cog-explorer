d3e86946e36264be171dc63f240984fda57381ff
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2018-04-20 09:24:58 +0200

    Adding actions to manage steps

--- src/actions/scenes/index.js
+++ src/actions/scenes/index.js
@@ -36,7 +36,7 @@ export function removeScene(url) {
   };
 }
 
-export function addSceneFromIndex(url) {
+export function addSceneFromIndex(url, pipeline = []) {
   return async (dispatch) => {
     dispatch(startLoading());
     try {
@@ -53,11 +53,30 @@ export function addSceneFromIndex(url) {
       const bands = files.filter(file => /LC0?8.*B[0-9]+.TIF$/.test(file));
 
       // TODO unset loading
-      dispatch(addScene(url, bands, red, green, blue));
+      dispatch(addScene(url, bands, red, green, blue, pipeline));
     } catch (e) {
       console.error(e);
     } finally {
       dispatch(stopLoading());
     }
+  };
+}
+
+export function addStep(url, operation) {
+  return {
+    type: SCENE_PIPELINE_ADD_STEP,
+    sceneId: url,
+    payload: {
+      operation,
+    },
+  };
+}
+
+export function editStep(url, index, payload) {
+  return {
+    type: SCENE_PIPELINE_EDIT_STEP,
+    sceneId: url,
+    index,
+    payload,
   };
 }
