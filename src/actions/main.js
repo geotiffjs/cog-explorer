3993955820c84ca6f8e9a6112946b1de56cad89f
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2018-04-25 17:18:15 +0200

    Adding actions, types and reducers for tileloading.

--- src/actions/main.js
+++ src/actions/main.js
@@ -1,6 +1,6 @@
 import types from '../types';
 
-const { START_LOADING, STOP_LOADING } = types;
+const { START_LOADING, STOP_LOADING, TILE_START_LOADING, TILE_STOP_LOADING } = types;
 
 export function startLoading() {
   return { type: START_LOADING };
@@ -8,4 +8,12 @@ export function startLoading() {
 
 export function stopLoading() {
   return { type: STOP_LOADING };
+}
+
+export function tileStartLoading() {
+  return { type: TILE_START_LOADING };
+}
+
+export function tileStopLoading() {
+  return { type: TILE_STOP_LOADING };
 }
--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,8 +1,8 @@
 import types from '../types';
 
-const { START_LOADING, STOP_LOADING } = types;
+const { START_LOADING, STOP_LOADING, TILE_START_LOADING, TILE_STOP_LOADING } = types;
 
-const initialState = { isLoading: false };
+const initialState = { isLoading: false, tilesLoading: 0 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
@@ -10,6 +10,10 @@ export default function (state = initialState, action) {
       return { ...state, isLoading: true };
     case STOP_LOADING:
       return { ...state, isLoading: false };
+    case TILE_START_LOADING:
+      return { ...state, tilesLoading: state.tilesLoading + 1 };
+    case TILE_STOP_LOADING:
+      return { ...state, tilesLoading: state.tilesLoading - 1 };
     default:
       return state;
   }
